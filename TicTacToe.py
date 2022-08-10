54a4ac749512a1698deaa5cbbbe03baa55ca2e7b
Lawal Abdulganiy <110787491+Abdulganiylawal@users.noreply.github.com>
2022-08-10 16:53:26 +0100

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -122,7 +122,7 @@ def gameOn():
             or board[0][0] == board[1][2] == board[2][4] == "o"
             or board[0][4] == board[1][2] == board[2][0] == "o"
         ):
-            if player1.turn == "0":
+            if player1.turn == "o":
                 print("The winner is {}".format(player1.name))
                 break
             elif player2.turn == "o":
